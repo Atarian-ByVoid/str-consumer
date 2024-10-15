c0eed80324f35f67076f42ee99fb4b47e20b4820
Carlos <cgomespimentel7@gmail.com>
2024-10-15 11:40:01 -0300

    feat: Tratamento de excessoes no kafka

--- src/main/java/com/beto/str_consumer/config/StringConsumerConfig.java
+++ src/main/java/com/beto/str_consumer/config/StringConsumerConfig.java
@@ -10,9 +10,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.RecordInterceptor;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @Configuration
 @RequiredArgsConstructor
 public class StringConsumerConfig {
@@ -35,5 +38,30 @@ public class StringConsumerConfig {
         factory.setConsumerFactory(consumerFactory);
         return factory;
     }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> validMessageContainerFactory(ConsumerFactory<String, String> consumerFactory){
+        var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.setRecordInterceptor(validMessage());
+        return factory;
+    }
+
+
+    //Valida mensagem no momento que a mensagem chega no kafka
+    private RecordInterceptor<String, String> validMessage() {
+        return (record, consumer) -> {
+            if (record.value().contains("Teste")) {
+                log.info("Possui a palavra Teste");
+            }else{
+                log.info("Não possui a palavra Teste");
+            }
+            return record;
+        };
+    }
+    
+    
+
+  
     
 }
--- src/main/java/com/beto/str_consumer/listeners/StrConsumerListener.java
+++ src/main/java/com/beto/str_consumer/listeners/StrConsumerListener.java
@@ -4,18 +4,21 @@ import org.springframework.stereotype.Component;
 
 import com.beto.str_consumer.custom.StrConsumerCustomListener;
 
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
     
+    @SneakyThrows // Throw do Lombok
     @StrConsumerCustomListener(groupdId = "group-1")
     public void listener(String message){
         log.info("CRIADO ::: Mensagem Recebida {}", message);
+        throw new IllegalArgumentException("EXECEPTION...");
     }
 
-    @StrConsumerCustomListener(groupdId = "group-2")
+    @StrConsumerCustomListener(groupdId = "group-2", containerFactory = "validMessageContainerFactory")
     public void log(String message){
         log.info("LOG ::: Mensagem Recebida {}", message);
     }
