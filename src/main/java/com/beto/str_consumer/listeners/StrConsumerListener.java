1dce35ab3c477d8226f6a53e23838d97e5d03dd0
Carlos <cgomespimentel7@gmail.com>
2024-10-15 09:15:45 -0300

    listener customizado

--- src/main/java/com/beto/str_consumer/listeners/StrConsumerListener.java
+++ src/main/java/com/beto/str_consumer/listeners/StrConsumerListener.java
@@ -1,7 +1,8 @@
 package com.beto.str_consumer.listeners;
 
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import com.beto.str_consumer.custom.StrConsumerCustomListener;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -9,12 +10,12 @@ import lombok.extern.log4j.Log4j2;
 @Component
 public class StrConsumerListener {
     
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupdId = "group-1")
     public void listener(String message){
         log.info("CRIADO ::: Mensagem Recebida {}", message);
     }
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupdId = "group-2")
     public void log(String message){
         log.info("LOG ::: Mensagem Recebida {}", message);
     }
